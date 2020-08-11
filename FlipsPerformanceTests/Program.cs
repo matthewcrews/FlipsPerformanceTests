38eda15497a2697366eb0c8e20b67bbc90bd2f65
Matthew Crews <matthewcrews@gmail.com>
2020-08-11 11:35:13 -0700

    adding tests

--- FlipsPerformanceTests/Program.cs
+++ FlipsPerformanceTests/Program.cs
@@ -1,12 +1,52 @@
 ﻿using System;
+using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Running;
+using Flips.Types;
 
 namespace FlipsPerformanceTests
 {
-    class Program
+    public class Program
     {
+
+        //[Params(10, 100, 1000, 10000)]
+        //public int NumberToAdd
+        //{
+        //    get; set;
+        //}
+
+        //[Benchmark]
+        //public LinearExpression AdditionTest()
+        //{
+        //    return LinearExpressionTests.PerfTests.addition(NumberToAdd);
+        //}
+
+        [Params(10, 100, 1000, 10000, 90900)]
+        public int StartIndex
+        {
+            get; set;
+        }
+
+        //[Benchmark]
+        //public Flips.SliceMap.SMap<int, LinearExpression> SliceMapSliceAndSumTest()
+        //{
+        //    return LinearExpressionTests.PerfTests.sliceMapGreaterThanMultiplication(StartIndex);
+        //}
+
+        [Benchmark]
+        public Flips.SliceMap.SMap<int,LinearExpression> SliceMapLessThanMultiplication()
+        {
+            return LinearExpressionTests.PerfTests.sliceMapLessThanMultiplication(StartIndex);
+        }
+
+        [Benchmark]
+        public LinearExpression SliceMapSumming()
+        {
+            return LinearExpressionTests.PerfTests.sliceMapSumming();
+        }
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            BenchmarkRunner.Run<Program>();
         }
     }
 }
---
+++ LinearExpressionTests/Library.fs
@@ -0,0 +1,55 @@
+﻿namespace LinearExpressionTests
+
+open Flips
+open Flips.Types
+open Flips.SliceMap
+
+module PerfTests =
+
+    let numberOfNames = 1_000
+    let numberOfExprs = 1_000
+    let numberOfCoefficients = 1_000_000
+    let maxExprSize = 10
+    let rng = new System.Random()
+    let decisions =
+        [|1 .. numberOfNames|]
+        |> Array.map string
+        |> Array.map (fun x -> Decision.createContinuous x 0.0 infinity)
+
+    let generateExpr (decisions:Decision []) (rng:System.Random) (maxExpressionSize:int) =
+        let numberOfElements = rng.Next(1, maxExpressionSize)
+
+        [|1 .. numberOfElements|]
+        |> Array.sumBy (fun x -> rng.NextDouble() * 1_000_000.0 * decisions.[rng.Next(0, decisions.Length - 1)])
+
+    let exprs =
+        [|for _ in 1 .. numberOfNames -> generateExpr decisions rng maxExprSize|]
+
+
+    let addition numberToAdd =
+        
+        [|for _ in 1 .. numberToAdd -> exprs.[rng.Next(0,numberOfExprs - 1)]|]
+        |> Array.sum
+
+
+    let coefficients =
+        [|for i in 1..numberOfCoefficients -> i, rng.NextDouble() * 1_000_000.0 |]
+        |> SliceMap.SMap.ofArray
+        
+    let decisionMap =
+        [|for i in 1..numberOfCoefficients -> i, (Decision.createContinuous (sprintf "%i" i) 0.0 infinity)|]
+        |> SliceMap.SMap.ofArray
+
+    //let sliceRng = new System.Random(123)
+
+    //let sliceMapGreaterThanMultiplication (startIndex) =
+    //    coefficients.[GreaterOrEqual (startIndex)] .* decisionMap
+
+    let slicing (startIndex) =
+        
+
+    let sliceMapLessThanMultiplication (startIndex) =
+        coefficients.[LessOrEqual (startIndex)] .* decisionMap
+
+    let sliceMapSumming () =
+        sum (coefficients .* decisionMap)
